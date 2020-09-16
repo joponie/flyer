87faef05029feb41c227ec354cbbc6defaddb63e
liujiepeng <liujiepeng@qudian.com>
2020-09-16 17:45:02 +0800

    init

--- web-base/src/main/java/jie/flyer/common/base/BaseController.java
+++ web-base/src/main/java/jie/flyer/common/base/BaseController.java
@@ -1,5 +1,6 @@
 package jie.flyer.common.base;
 
+import jie.flyer.common.base.vo.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,4 +14,11 @@ public abstract class BaseController {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+    protected Response success() {
+        return Response.ok();
+    }
+
+    protected Response success(Object o) {
+        return Response.ok(o);
+    }
 }
