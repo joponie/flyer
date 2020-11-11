ba450e3df960768a5d045d3b5965a2a099936028
liujiepeng <liujiepeng@qudian.com>
2020-11-11 16:09:19 +0800

    init

--- flyer-web/doc/init.sql
+++ flyer-web/doc/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE `user`
     `update_by`   int(11) unsigned    NOT NULL DEFAULT '0' COMMENT '更新人ID',
     `create_time` timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `delFlg`      tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '删除标记：0=未删除；1=已删除',
+    `del_flg`      tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '删除标记：0=未删除；1=已删除',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='用户';
